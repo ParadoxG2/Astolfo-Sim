6762a748a48123af1228c765d4f08b8cd7882837
Paradox <ttvparadoxg@gmail.com>
2024-09-05 07:50:07 -0500

    the good vershion

--- core/src/main/java/io/github/Paradox/LaunchScreen.java
+++ core/src/main/java/io/github/Paradox/LaunchScreen.java
@@ -1,10 +1,13 @@
 package io.github.Paradox;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.awt.*;
@@ -12,65 +15,77 @@ import java.awt.*;
 /** First screen of the application. Displayed after the application is created. */
 public class LaunchScreen implements Screen {
 
+    // Game Window
     final Main game;
 
-    Texture Logo;
+    // Refs
     OrthographicCamera camera;
+    Texture Logo;
 
-    public LaunchScreen(final Main game) {
+
+    // Screen Setup
+    public LaunchScreen(final Main game){
         this.game = game;
 
-        //Load imiges
+        //init camera
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, game.dm.width, game.dm.height);
+
+        // init assets
         Logo = new Texture(Gdx.files.internal("logo.png"));
 
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
     }
 
     @Override
-    public void render(float delta){
+    public void render(float delta) {
         ScreenUtils.clear(0, 0, 0.2f, 1);
 
+
+
+        //camera main code
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
-        game.font.draw(game.batch, "ParadoxInteractive llc", 100, 100);
-        game.batch.draw(Logo, 0, 0, 300, 100);
-
+        game.batch.draw(Logo, 0, 0, 1066,  433 );
         game.batch.end();
 
-        if(Gdx.input.isTouched()){
-            game.setScreen(new GameScreen(game));
-            dispose();
-        }
-    }
+        game.shape.setProjectionMatrix(camera.combined);
+        game.shape.begin(ShapeRenderer.ShapeType.Filled);
+        game.shape.setColor(Color.CYAN);
+        game.shape.rect(0, 0, game.dm.width, game.dm.height);
+        game.shape.end();
 
-    @Override
-    public void resize(int width, int height) {
+
     }
 
     @Override
     public void show() {
-
+        // Prepare your screen here.
     }
 
     @Override
-    public void hide() {
+    public void resize(int width, int height) {
+        // Resize your screen here. The parameters represent the new window size.
     }
 
     @Override
     public void pause() {
+        // Invoked when your application is paused.
     }
 
     @Override
     public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void hide() {
+        // This method is called when another screen replaces this one.
     }
 
     @Override
     public void dispose() {
-        Logo.dispose();
-        game.batch.dispose();
+        // Destroy screen's assets here.
     }
-
 }
--- core/src/main/java/io/github/Paradox/Main.java
+++ core/src/main/java/io/github/Paradox/Main.java
@@ -1,18 +1,30 @@
 package io.github.Paradox;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
-    public SpriteBatch batch;
+   public SpriteBatch batch;
     public BitmapFont font;
+    public Skin Launchskin;
+    public Graphics.DisplayMode dm;
+    public ShapeRenderer shape;
 
     public void create() {
-        batch = new SpriteBatch();
+        dm = Gdx.graphics.getDisplayMode();
+        Gdx.graphics.setWindowedMode(dm.width, dm.height);
+        batch = new SpriteBatch(); // Spright Batch main
         font = new BitmapFont(); // use libGDX's default Arial font
+        Launchskin = new Skin();
         this.setScreen(new LaunchScreen(this));
+
     }
 
     public void render() {
